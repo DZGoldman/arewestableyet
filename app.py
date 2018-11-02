2e80a605a56bbf01da17550af7011d8e4789f9b3
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-11-02 13:07:52 -0400

    optinal debugger for dev

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ def home():
 
 def index():
     '''Return index.html for all non-api routes'''
-    return render_template( 'index.html')
+    return render_template( 'index.html') 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEV'))
 
 
